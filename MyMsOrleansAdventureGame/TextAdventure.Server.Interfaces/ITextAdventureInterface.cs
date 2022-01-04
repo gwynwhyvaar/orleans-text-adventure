c9580abf9ec0eebd1e3e1d756e005ee414adc103
Ayotunde Ayoko <ayotunde.ayoko@un.org>
2022-01-04 16:46:36 +0300

    Added a Server implementation

---
+++ MyMsOrleansAdventureGame/TextAdventure.Server.Interfaces/ITextAdventureInterface.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+using TextAdventure.Entities;
+using TextAdventure.Grain.Interfaces;
+
+namespace TextAdventure.Server.Interfaces
+{
+    public interface ITextAdventureInterface
+    {
+        Task<IRoomGrain> MakeRoomAsync(RoomInfo data);
+        Task MakeThingAsync(ThingInfo thing);
+        Task MakeMonsterAsync(MonsterInfo data, IRoomGrain room);
+        Task ConfigureAsync(string fileName);
+    }
+}
---
+++ MyMsOrleansAdventureGame/TextAdventure.Server/TextAdventureServer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Orleans;
+
+using TextAdventure.Entities;
+using TextAdventure.Grain.Interfaces;
+using TextAdventure.Server.Interfaces;
+
+namespace TextAdventure.Server
+{
+    public class TextAdventureServer : ITextAdventureInterface
+    {
+        private readonly IGrainFactory _grainFactory;
+        public TextAdventureServer(IGrainFactory grainFactory)
+        {
+            this._grainFactory = grainFactory;
+        }
+        public async Task ConfigureAsync(string fileName)
+        {
+            var rand = new Random();
+            var jsonData = await File.ReadAllTextAsync(fileName);
+            var data = await Task.Run(() => JsonSerializer.Deserialize<MapInfo>(jsonData));
+            // init the game world with game data
+            var rooms = new List<IRoomGrain>();
+            var gameRooms = data.Rooms;
+            var gameThings = data.Things;
+            var gameMonsters = data.Monsters;
+            foreach (var room in gameRooms)
+            {
+                var roomGrain = await MakeRoomAsync(room);
+                if (room.Id >= 0)
+                {
+                    rooms.Add(roomGrain);
+                }
+            }
+            foreach (var thing in gameThings)
+            {
+                await MakeThingAsync(thing);
+            }
+            foreach (var monster in gameMonsters)
+            {
+                await MakeMonsterAsync(monster, rooms[rand.Next(0, rooms.Count)]);
+            }
+        }
+
+        public async Task MakeMonsterAsync(MonsterInfo data, IRoomGrain room)
+        {
+            var monsterGrain = this._grainFactory.GetGrain<IMonsterGrain>(data.Id);
+            await monsterGrain.SetInfoAsync(data);
+            await monsterGrain.SetRoomGrainAsync(room);
+        }
+
+        public async Task<IRoomGrain> MakeRoomAsync(RoomInfo data)
+        {
+            var roomGrain = this._grainFactory.GetGrain<IRoomGrain>(data.Id);
+            await roomGrain.SetInfoAsync(data);
+            return roomGrain;
+        }
+
+        public async Task MakeThingAsync(ThingInfo thing)
+        {
+            var roomGrain = this._grainFactory.GetGrain<IRoomGrain>(thing.FoundIn);
+            await roomGrain.DropAsync(thing);
+        }
+    }
+}
