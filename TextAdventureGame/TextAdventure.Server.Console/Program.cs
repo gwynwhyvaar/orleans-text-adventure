b6c503a1e30194d5c3dacdd3afd529f05bbd47c6
Ayotunde Ayoko <ayotunde.ayoko@un.org>
2022-01-05 10:10:31 +0300

    renamed source folder

---
+++ TextAdventureGame/TextAdventure.Server.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace TextAdventure.Server.Console
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var mapFileName = Path.Combine(path, "TextAdventureMap.json");
+            switch (args.Length)
+            {
+                default:
+                    System.Console.WriteLine("Invalid Command Line Arguments.");
+                    return -1;
+                case 0:
+                    break;
+                case 1:
+                    mapFileName = args[0];
+                    break;
+            }
+            if (File.Exists(mapFileName))
+            {
+                System.Console.WriteLine($"File not found: {mapFileName}");
+                return -2;
+            }
+        }
+    }
+}
